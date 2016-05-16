d174e068f2cfb8c36b3638c4e5209d6d95417ee8
s-sirchia <s-sirchia@github.com>
2016-05-16 15:44:21 +0200

    Updated dataset generators

---
+++ Script/GenDataset/Test_Parser.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+
+from Parser import read_wibbi
+import json
+import random
+
+def set_default(obj):
+	if isinstance(obj,set):
+		return list(obj)
+	raise TypeError
+
+def create_link(dataset):
+    for name1 in dataset:
+        for name2 in dataset:
+           if name1!=name2:
+
+               U = random.sample(dataset[name1]["graph"].keys(),10)
+               V = random.sample(dataset[name2]["graph"].keys(),10)
+
+               for i in range(len(U)):
+                   if V[i] not in dataset[name1]["graph"][U[i]]: 
+                       dataset[name1]["graph"][U[i]].add(V[i])
+ 
+
+siti = ["ARTS","BUSINESS","COMPUTERS","GAMES","HEALTH","HOME","KIDSANDTEENS","NEWS","RECREATION","REFERENCE","REGIONAL",
+"SCIENCE","SHOPPING","SOCIETY","SPORTS"]
+
+siti = ["SCIENCE"]
+
+dataset = dict()
+for name in siti:
+	print("Parsing: " + name)
+	graph,db = read_wibbi(name+".pages")
+	dataset[name] = {"graph":graph,"db":db}
+	print("----------------")
+
+print("------FINISHED PARSING-----")
+
+
+print("------CREATING FAKE LINK -----")
+create_link(dataset)
+print("------FINISHED CREATING-------")
+
+print("------START DUMPING-----")
+with open('dataset.json', 'w') as fp:
+        stri = json.dumps(dataset, ensure_ascii=False, encoding="utf-8", default = set_default)
+        fp.write(stri)
+print("------DONE-----")
+
+
+
+
