a909644dd30431664855fdf7aa91a71b3af2a4a1
Abelarm <luigi3000@gmail.com>
2016-05-19 11:45:09 +0200

    Added Time comparison

--- Script/Test_HITS.py
+++ Script/Test_HITS.py
@@ -1,14 +1,17 @@
-from HITS import HITS2
+from HITS import *
+import timeit
+
 
 simple = [[0, 1, 1, 1, 0], [1, 0, 0, 1, 0], [0, 0, 0, 0, 1], [0, 1, 1, 0, 0], [0, 0, 0, 0, 0]]
 
-'''
+
+start_time = timeit.default_timer()
 time , a, h = HITS1(simple,1000)
+elapsed = timeit.default_timer() - start_time
+print("HITS1 elapsed: " + str(elapsed))
 print("Time:" + str(time))
 print ("A: " + str(a))
 print ("H: "+ str(h))
-'''
-
 
 # Graph is represented with its adjacency lists
 simple = dict()
@@ -18,8 +21,10 @@ simple['z'] = {'k'}
 simple['w'] = {'y','z'}
 simple['k'] = {}
 
-
+start_time = timeit.default_timer()
 time , a, h = HITS2(simple,1000)
+elapsed= timeit.default_timer() - start_time
+print("HITS2 elapsed: " + str(elapsed))
 print("Time:" + str(time))
 print ("A: " + str(a))
 print ("H: "+ str(h))
