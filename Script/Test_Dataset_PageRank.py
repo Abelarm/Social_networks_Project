f5fd129143c68dae30500582871742dbcd38cd27
Abelarm <luigi3000@gmail.com>
2016-05-29 18:24:39 +0200

    BugFix

--- Script/Test_Dataset_PageRank.py
+++ Script/Test_Dataset_PageRank.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
 from PageRank import pageRank2
-from load_dataset import graph
+from load_dataset import get_graph
 import timeit
 
-graph = graph()
+graph = get_graph()
 start_time = timeit.default_timer()
 time2, rank2 = pageRank2(graph,0.85,75,0)
 elapsed2 = timeit.default_timer() - start_time
