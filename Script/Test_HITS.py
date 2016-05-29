71270f3045198d62e792ea67b81eca3e24d7bc51
Abelarm <luigi3000@gmail.com>
2016-05-29 19:02:11 +0200

    Added Dumping of HITS result

--- Script/Test_HITS.py
+++ Script/Test_HITS.py
@@ -10,5 +10,18 @@ time , a, h = HITS2(graph,1000,0.001)
 elapsed= timeit.default_timer() - start_time
 print("HITS2 elapsed: " + str(elapsed))
 print("Time:" + str(time))
+
+
+result = dict()
+
+for k in graph:
+	result[k] = {"a": a[k], "h": h[k]}
+
+print("---------DUMPING RESULT---------")
+with open('HITS.json','w') as fp:
+	json.dump(result,fp)
+print("---------FINISHED DUMPING---------")
+
+
 #print ("A: " + str(a))
 #print ("H: "+ str(h))
