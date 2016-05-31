65f61f773b98fc72014d71b0a8c49f80568d04c0
s.sirchia <ste.sirchia@gmail.com>
2016-05-31 16:55:23 +0200

    Added time registration of search engine methods

--- Script/SearchEngine.py
+++ Script/SearchEngine.py
@@ -4,6 +4,7 @@ from Dataset_Matching import *
 import pprint
 import operator
 from collections import OrderedDict
+import timeit
 
 def order_by_PageRank(result, PageRank_graph):
     temp = dict()
@@ -20,9 +21,18 @@ def order_by_HITS(result, HITS_graph, tp):
     return resultH
 
 print("-------------LOADING FULL DATASET-------------")
+start_time = timeit.default_timer()
 inverted_db = get_inverted()
+elapsed = timeit.default_timer() - start_time
+print("Database loading time: "+str(elapsed))
+start_time = timeit.default_timer()
 PageRank_graph = get_PageRank_graph()
+elapsed = timeit.default_timer() - start_time
+print("PageRank graph loading time: "+str(elapsed))
+start_time = timeit.default_timer()
 HITS_graph = get_HITS_graph()
+elapsed = timeit.default_timer() - start_time
+print("HITS loading time: "+str(elapsed))
 print("---------------------------------------------")
 
 queries = []
@@ -38,27 +48,45 @@ for query in queries:
     print(query)
 
     print("------------------BEST MATCH-----------------")
+    start_time = timeit.default_timer()
     result = best_match(query, 0, inverted_db)
+    elapsed = timeit.default_timer() - start_time
+    print("Best Match time: "+str(elapsed))
     pprint.pprint(str(result))
-    '''
+    
     print("--------BEST MATCH ORDERED BY PAGERANK-------")
+    start_time = timeit.default_timer()
     resultPG = order_by_PageRank(result, PageRank_graph)
+    elapsed = timeit.default_timer() - start_time
+    print("Best Match Pagerank ordering time: "+str(elapsed))
     pprint.pprint(str(resultPG))
-    '''
+    
     print("----------BEST MATCH ORDERED BY HITS---------")
+    start_time = timeit.default_timer()
     resultH = order_by_HITS(result, HITS_graph,"a")
+    elapsed = timeit.default_timer() - start_time
+    print("Best Match HITS ordering time: "+str(elapsed))
     pprint.pprint(str(resultH))
     
     print("-------------IMPROVED BEST MATCH-------------")
+    start_time = timeit.default_timer()
     result2 = improved_best_match(query, inverted_db)
+    elapsed = timeit.default_timer() - start_time
+    print("Improved Best Match time: "+str(elapsed))
     pprint.pprint(str(result2))
-    '''
+    
     print("---IMPROVED BEST MATCH ORDERED BY PAGERANK---")
+    start_time = timeit.default_timer()
     result2PG = order_by_PageRank(result2, PageRank_graph)
+    elapsed = timeit.default_timer() - start_time
+    print("Improved Best Match Pagerank ordering time: "+str(elapsed))
     pprint.pprint(str(result2PG))
-    '''
+    
     print("-----IMPROVED BEST MATCH ORDERED BY HITS-----")
+    start_time = timeit.default_timer()
     result2H = order_by_HITS(result2, HITS_graph,"a")
+    elapsed = timeit.default_timer() - start_time
+    print("Improved Best Match HITS ordering time: "+str(elapsed))
     pprint.pprint(str(result2H))
 
     print("---------------------------------------------")
