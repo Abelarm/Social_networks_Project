2ec00fb34198a987ac78c07ec730440ce01e900d
Abelarm <luigi3000@gmail.com>
2016-06-05 18:38:35 +0200

    Added parallel version of HITS

--- Script/HITS.py
+++ Script/HITS.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 from load_dataset import get_fullgraph
+from joblib import Parallel, delayed
 
 def HITS1(graph,step,confidence=1.0e-6):
   n = len(graph)
@@ -68,11 +69,11 @@ def HITS2(graph,step,confidence=1.0e-6):
     time += 1
     
     maxA = -1
-
-
     for n in nodes:
       incoming = graph[n]["incoming"]
       a[n] = 0
+      if type(incoming) == float:
+        incoming=[]
       for i in incoming:
           if i in h:
             a[n] += h[i]
@@ -126,6 +127,146 @@ def HITS2(graph,step,confidence=1.0e-6):
     olddiffa = diffa
     olddiffh = diffh
 
-    #print("Iteration: " + str(time) +"/" +str(step) + " diffa: " + str(diffa) + " diffh " + str(diffh))
+    print("Iteration: " + str(time) +"/" +str(step) + " diffa: " + str(diffa) + " diffh " + str(diffh))
     
+  return time, a, h
+
+
+def calculate_a(list_key, graph, h):
+
+  a = dict()
+  
+  print("entrato nel metodo a")
+  maxA = -1
+  for n in list_key:
+    incoming = graph[n]["incoming"]
+    a[n] = 0
+    if type(incoming) == float:
+      incoming=[]
+    for i in incoming:
+        if i in h:
+          a[n] += h[i]
+    if a[n] > maxA:
+      maxA = a[n] 
+
+  print("esco da metodo a")
+  return maxA, a
+
+def calculate_h(list_key, graph, a):
+
+  h = dict()
+  print("entrato nel metodo h")
+  maxH = -1 
+  for n in list_key:
+    outgoing = graph[n]["outgoing"]
+    h[n] = 0
+    if type(outgoing) == float:
+      outgoing=[]
+    for o in outgoing:
+      if o in a:
+        h[n] += a[o]
+    if h[n] > maxH:
+      maxH = h[n]
+
+  print("esco dal metodo h")
+  return maxH, h
+
+
+def HITS_parallel(graph,step,confidence=1.0e-6):
+
+  num_procs = 4
+  nodes=graph.keys()
+  num_nodes=len(nodes)
+  done = 0
+  time = 0
+  
+  #Initialization
+  a = dict()
+  h = dict()
+  lastA = dict()
+  lastH = dict()
+  olddiffa = 0
+  olddiffh = 0
+  for i in nodes:
+    a[i] = 0.0
+    lastA[i] = 0.0
+    h[i] = 1.0 
+    lastH[i] = 0.0
+
+  with Parallel(n_jobs = num_procs) as parallel:
+  
+    while not done and time < step:
+      #print("Step: " + str(time)+"/"+str(step))
+      time += 1
+
+      split_keys = lambda lst,sz: [lst[i:i + sz] for i in range(0, len(lst), sz)]
+
+      chunk_size = int(num_nodes/num_procs)
+
+      keys = split_keys(list(graph.keys()), chunk_size)
+
+      print(len(keys))
+      
+      print("cerca di calcolare a")
+      result_a = parallel(delayed(calculate_a)(keys[i], graph, h) for i in range(len(keys)))
+      print("finito di calcolare a")
+      #print (result_a)
+
+      maxA = max([result_a[i][0] for i in range(num_procs)])
+
+      a = dict()
+
+      print ("update a")
+      for i in range(num_procs):
+        a.update(result_a[i][1])
+      print("finished")
+
+      for n in a:
+        a[n] /= maxA
+
+      print("cerca di calcolare h")
+      result_h = parallel(delayed(calculate_h)(keys[i], graph,  a) for i in range(len(keys)))
+      print("finito di calcolare h")
+
+      maxH = max([result_h[i][0] for i in range(num_procs)])
+
+      h = dict()
+
+      print ("update h")
+      for i in range(num_procs):
+        h.update(result_h[i][1])
+      print("finished")
+
+      for n in h:
+        h[n] /= maxH
+
+      #print("A: " +str(a))
+      #print("H: " +str(h))
+      #raw_input()
+
+      #Computes the distance between the old rank vector and the new rank vector in L_1 norm
+
+      npa = np.array(list(a.values()))
+      nph = np.array(list(h.values()))
+      nplastA = np.array(list(lastA.values()))
+      nplastH = np.array(list(lastH.values()))
+
+      diffa = np.abs(npa - nplastA).sum()
+      diffh = np.abs(nph - nplastH).sum()
+      lastA = a.copy()
+      lastH = h.copy()
+
+      #print(float(olddiffa)-diffa)
+
+      if diffa <= confidence or diffh < confidence:
+          done = 1
+
+      if np.abs(olddiffa-diffa) <= confidence or np.abs(olddiffh-diffh) <= confidence:
+          done = 1
+
+      olddiffa = diffa
+      olddiffh = diffh
+
+      print("Iteration: " + str(time) +"/" +str(step) + " diffa: " + str(diffa) + " diffh " + str(diffh))
+
   return time, a, h
