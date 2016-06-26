4eced4dc96d8301d3589533a3191943297b5a702
s.sirchia <ste.sirchia@gmail.com>
2016-06-26 19:54:12 +0200

    Bugs, please stop happening

--- Script/Test_Dataset_PageRank.py
+++ Script/Test_Dataset_PageRank.py
@@ -9,12 +9,12 @@ rep = 1
 elapsed_load = 0
 elapsed_dump = 0
 elapsed = 0
+
+start_time_load = timeit.default_timer()
+graph = get_graph()
+elapsed_load += timeit.default_timer() - start_time_load 
+
 for i in range(rep):
-
-	start_time_load = timeit.default_timer()
-	graph = get_graph()
-	elapsed_load += timeit.default_timer() - start_time_load 
-
 	start_time = timeit.default_timer()
 	#print("---------STARTING PAGERANK---------")
 	time2, rank2 = pageRank2(graph,0.85,75,1e-4)
